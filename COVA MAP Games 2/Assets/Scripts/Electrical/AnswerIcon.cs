0c1b91bafdd9d1aa93dcf6c097f8936128a6afd6
Aconnollyodu <aconn008@odu.edu>
2025-01-14 18:31:24 -0500

    work completed 1/14/25 pt.2
    rest of work completed on 1/14/25 -AC

---
+++ COVA MAP Games 2/Assets/Scripts/Electrical/AnswerIcon.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class AnswerIcon : MonoBehaviour
+{
+     public GameObject correctSprite; // Drag the "Correct" sprite here
+    public GameObject incorrectSprite; // Drag the "Incorrect" sprite here
+
+    // Call this method with the answer's correctness
+    public void ShowAnswerIcon(bool isCorrect)
+    {
+        // Deactivate both sprites first
+        correctSprite.SetActive(false);
+        incorrectSprite.SetActive(false);
+
+        // Activate the appropriate sprite based on the answer
+        if (isCorrect)
+        {
+            correctSprite.SetActive(true);
+        }
+        else
+        {
+            incorrectSprite.SetActive(true);
+        }
+
+        // Optionally, hide the feedback after a delay
+        Invoke("HideFeedback", 2f); // Hide after 2 seconds
+    }
+
+    // Hide both sprites
+    private void HideFeedback()
+    {
+        correctSprite.SetActive(false);
+        incorrectSprite.SetActive(false);
+    }
+}
