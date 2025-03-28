ae27e9907b42369730d6a56430ed03f115ada899
AConnolly <auconnolly@gmail.com>
2025-03-28 17:42:39 -0400

    all work completed 3/28/25 -AC
    scene creation and some script creation, most work pertained to layout and placeholders. and setting up the project/scene.

---
+++ COVA MAP Games 2/Assets/Scripts/Pipefitter/PipefitterpartData.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using FuzzPhyte.Utility;
+
+
+
+
+[CreateAssetMenu(fileName ="CorrectPartData", menuName = "Pipefitter/CorrectPartData")]
+public class PipefitterpartData : FP_Data
+{
+    [Header("Part_Name")]
+
+
+    [Header("Part_Specs")]
+
+
+    [Header("Logic")]
+
+
+    [Header("Checklist parameters")]
+    public bool beenWelded;
+}
