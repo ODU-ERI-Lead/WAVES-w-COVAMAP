63349c8427daf7f51e1073cc9058a46f85ff622f
AConnolly <auconnolly@gmail.com>
2025-04-04 17:00:50 -0400

    all work done 4/4/25 -AC
    cutting tool using shader graphs created but not finalized need scripting, button for blueprint needs some fixing and other minor changes were done all throughout. Some sounds were added and other things filled out like Johns UI manager but needs some fixing.

--- COVA MAP Games 2/Assets/Scripts/Pipefitter/BluePrint Button.cs
+++ COVA MAP Games 2/Assets/Scripts/Pipefitter/BluePrint Button.cs
@@ -1,13 +1,59 @@
+
 using UnityEngine;
 using UnityEngine.UI;
 
 public class BluePrintButton : MonoBehaviour
 {
     public Button BluePrint_Button;
+    public bool BP_Pressed = false;
     public Button BP_close;
-    public Image BluePrint_Image;
+   // public Toggle 
+    public GameObject BluePrint_Image;
+    //public Image BluePrint_Image;
+
+
+   /// public void ImageAlphaSetOne(bool alphaActive)
+    //{
+     //   var curColor = BluePrint_Image.color;
+     //   float alpha = 1f;
+     //   if (!alphaActive)
+      //  {
+     //       alpha = 0;
+     //   }
+    //    BluePrint_Image.color = new Color(curColor.r, curColor.g, curColor.b, alpha);
+  //  }
+    public void Onopen()
+    {
+       
+        if (BP_Pressed = true)
+        {
+            BluePrint_Image.SetActive(true);
+           
+        }
+    }
+
+    public void Onclose()
+    {
+        if (BP_Pressed = false)
+        {
+            Debug.Log("Button clicked, object is being deactivated");
+            BluePrint_Image.SetActive(false);
+           // might need one for close button as well 
+        }
+
+    }
 
 
 
+
+
+  //  public Onopen()
+  //  {
+   //     if ()
+     //   {
+     //       BP_Pressed = true;
+     //       this.BluePrint_Image = GameObject.active
+     //   }
+  //  }
 
 }
