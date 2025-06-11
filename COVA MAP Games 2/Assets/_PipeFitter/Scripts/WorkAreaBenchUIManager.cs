89d788d759fa8bae8f4d746921d9428daffa08af
John Shull <the.john.shull@gmail.com>
2025-06-10 20:57:45 -0400

    BluePrint Updates
    Fixing a MF BP

--- COVA MAP Games 2/Assets/_PipeFitter/Scripts/WorkAreaBenchUIManager.cs
+++ COVA MAP Games 2/Assets/_PipeFitter/Scripts/WorkAreaBenchUIManager.cs
@@ -29,10 +29,26 @@ public class WorkAreaBenchUIManager : MonoBehaviour
     public MoveCutParts MoveCutPartsRef;
     public PipeFitterMouseCutter MouseCutterRef;
     protected List<Button> allPartsButtons = new List<Button>();
+    [Space]
+    [Header("Blue Print Panel")]
+    public Camera BPCameraOverlay;
+    public Canvas BPCanvas;
+    public RectTransform BluePrintPanel;
+    public Button BluePrintReturnButton;
+    public Button BluePrintZoomInButton;
+    public Button BluePrintZoomOutButton;
+    public int MaxZoomInt = 5;
+    protected int zoomStartValue = 0;
+    public float ZoomPanelRatioX = 50;
+    private Vector2 originalAnchorMin;
+    private Vector2 originalAnchorMax;
+    private float zoomPanelRatioY { get { return ZoomPanelRatioX * 0.5625f; } } // 16:9 aspect ratio
     public void OnEnable()
     {
         MouseCutterRef.OnFininshedCutting += MouseCutterRef_OnFininshedCutting;
         StartCoroutine(YieldASecond());
+        originalAnchorMin = BluePrintPanel.anchorMin;
+        originalAnchorMax = BluePrintPanel.anchorMax;
     }
 
     private void MouseCutterRef_OnFininshedCutting()
@@ -174,6 +190,16 @@ public class WorkAreaBenchUIManager : MonoBehaviour
         CutButton.interactable = false;
         SendToWorkButton.interactable = false;
         RotationButton.interactable = false;
+        if (BPCanvas != null)
+        {
+            BPCanvas.gameObject.SetActive(true);
+            if (BluePrintPanel != null)
+            {
+                BluePrintPanel.gameObject.SetActive(true);
+                zoomStartValue = 0;
+                BluePrintSetCameraByZoom();
+            }
+        }
     }
     /// <summary>
     /// called from the home button on the work bench area (NOT THE BLUE PRINT HOME)
@@ -238,5 +264,53 @@ public class WorkAreaBenchUIManager : MonoBehaviour
             notMe.interactable = notMeState;
         }
         
+    }
+
+    /// <summary>
+    /// UI button to zoom in from the blueprint work assembly
+    /// </summary>
+    [ContextMenu("UI_Zoom in Test")]
+    public void UIBluePrintZoomIn()
+    {
+        zoomStartValue++;
+        if (zoomStartValue > MaxZoomInt)
+        {
+            zoomStartValue = MaxZoomInt;
+        }
+        BluePrintSetCameraByZoom();
+    }
+
+    /// <summary>
+    /// UI button to zoom out from the blueprint work assembly
+    /// </summary>
+    [ContextMenu("Zoom Out Test")]
+    public void UIBluePrintZoomOut()
+    {
+        zoomStartValue--;
+        if (zoomStartValue < 0)
+        {
+            zoomStartValue = 0;
+        }
+        BluePrintSetCameraByZoom();
+    }
+    protected void BluePrintSetCameraByZoom()
+    {
+        float screenWidth = Screen.width;
+        float screenHeight = Screen.height;
+
+        float zoomedWidthPixel = screenWidth + (zoomStartValue * ZoomPanelRatioX);
+        float zoomedHeightPixel = screenHeight + (zoomStartValue * zoomPanelRatioY);
+
+        float widthNorm =  zoomedWidthPixel/ screenWidth;
+        float heightNorm = zoomedHeightPixel/ screenHeight;
+
+        float anchorCenterX = (originalAnchorMin.x + originalAnchorMax.x) / 2f;
+        float anchorCenterY = (originalAnchorMin.y + originalAnchorMax.y) / 2f;
+
+        float newAnchorWidth = widthNorm;
+        float newAnchorHeight = heightNorm;
+
+        BluePrintPanel.anchorMin = new Vector2(anchorCenterX - newAnchorWidth / 2f, anchorCenterY - newAnchorHeight / 2f);
+        BluePrintPanel.anchorMax = new Vector2(anchorCenterX + newAnchorWidth / 2f, anchorCenterY + newAnchorHeight / 2f);
     }
 }
